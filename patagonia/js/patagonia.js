14091e7979c54e069d185e81e9fc934d9991e10b
meoritdol <jangchaeyn@gmail.com>
2024-06-13 13:57:57 +0900

    반응형 완성

---
+++ patagonia/js/patagonia.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  var menu = document.getElementById('fullscreenMenu');
+  menu.style.display = 'block';
+}
+
+function closeMenu() {
+  var menu = document.getElementById('fullscreenMenu');
+  menu.style.display = 'none';
+}
+
